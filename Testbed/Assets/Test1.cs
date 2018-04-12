378eb745ea65a45267a6e38a3b568330e5019d41
Mikal Saltveit <mikal@stage.co>
2018-04-11 17:53:28 -0700

    Fixed .gitignore

---
+++ Testbed/Assets/Test1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour 
+{
+	private Transform m_Transform;
+
+	// Use this for initialization
+	void Start () {
+		m_Transform = transform;//gameObject.GetComponent<Transform>( );
+
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
